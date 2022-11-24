309242db01d3c317b257f2cbb50016d6f9116420
Scott Davis <scott@qonqr.com>
2022-11-23 19:53:49 -0600

    fixed pinReader

--- src/pinReader.h
+++ src/pinReader.h
@@ -7,6 +7,8 @@ class PinReader
 {
 public:    
     void init() {
+        //Ardunio has a built in pull up resistor for digital inputs
+        //if the circuit is open, the pin will read high
         pinMode(8, INPUT_PULLUP);
         pinMode(9, INPUT_PULLUP);
         pinMode(10, INPUT_PULLUP);
@@ -15,32 +17,30 @@ public:
         pinMode(13, INPUT_PULLUP);
     }
 
-    int read(){
-        int val = 0;
-        if (digitalRead(8) == LOW)
-        {
-            val = val + 1;
+    //this will return 0 or 1
+    //1 indicates the switch was closed (on)
+    //0 indicates the switch was open (off)    
+    int checkPinIsClosed(int pin){
+        if (digitalRead(pin) == LOW) {
+            return 1;
+        } else {
+            return 0;
         }
-        if (digitalRead(9) == LOW)
-        {
-            val = val + 2;
-        }
-        if (digitalRead(10) == LOW)
-        {
-            val = val + 4;
-        }
-        if (digitalRead(11) == LOW)
-        {
-            val = val + 8;
-        }
-        if (digitalRead(12) == LOW)
-        {
-            val = val + 16;
-        }
-        if (digitalRead(13) == LOW)
-        {
-            val = val + 32;
-        }
+    }
+
+    int read(){               
+        int pin0 = checkPinIsClosed(8);
+        int pin1 = checkPinIsClosed(9);
+        int pin2 = checkPinIsClosed(10);
+        int pin3 = checkPinIsClosed(11);
+        int pin4 = checkPinIsClosed(12);
+        int pin5 = checkPinIsClosed(13);
+
+        //treat the pins as a binary number
+        //001011 in binary is 0 + 0 + 8 + 0 + 2 + 1 = 11 
+        int val =  (32 * pin5) + (16 * pin4) + (8 * pin3) + (4 * pin2) + (2 * pin1) + (1 * pin0);
+        
+        //the return value will be between 0 - 63
         return val;
     }
 };
