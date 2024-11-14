cadd2aaec18280b8d4a9e467914302f584fe6b1e
Augustus Schendel <116044645+Augustus-Schendel@users.noreply.github.com>
2024-11-13 19:05:37 -0600

    Update constants.h
    Changed Pin number on the off chance someone copies directly from the files rather than wiki

--- src/constants.h
+++ src/constants.h
@@ -2,7 +2,7 @@
 #define CONST_H
 
 #define NUM_LEDS 256 //how many LEDs are in your strip/panel
-#define DATA_PIN 3 //signal wire is connected to this pin on arduino
+#define DATA_PIN 2 //signal wire is connected to this pin on arduino
 #define COLOR_ORDER GRB //surprisingly, many LED strips don't use the color order Red,Green,Blue
 #define CHIP_SET WS2812B //KnightKrawler uses WS2812B Leds, but there are many other options
 #define BRIGHTNESS 255 //255 is maximum brightness, lower this value to limit brightness
